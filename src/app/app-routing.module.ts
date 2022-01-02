86039a817e11622145e0777c43ec474faeee935c
Shu Han <codershuhan@gmail.com>
2022-01-01 16:21:34 -0800

    stage 1 for description page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DescriptionComponent } from './views/description/description.component';
+import { RecordComponent } from './views/record/record.component';
+import { SummaryComponent } from './views/summary/summary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: DescriptionComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'record', component: RecordComponent },
+  { path: 'summary', component: SummaryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
